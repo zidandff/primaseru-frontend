c3f1d0d47b7145e540555b9dbb3f6659de0571b2
Zidan M. Daffa <zidandaff@gmail.com>
2021-07-22 15:44:23 +0700

    Add nav toggler animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const navbar = document.querySelector('.navbar');
+const navToggler = document.querySelector('.navbar-toggler');
 
 document.addEventListener('scroll', ()=> {
     const scrollTop = this.pageYOffset;
     if(scrollTop >= 50){
-        navbar.classList.add('scrolled')
+        navbar.classList.add('scrolled');
     } else {
-        navbar.classList.remove('scrolled')
+        navbar.classList.remove('scrolled');
     }
+});
+
+navToggler.addEventListener('click', ()=> {
+    navToggler.classList.toggle('active')
 })
